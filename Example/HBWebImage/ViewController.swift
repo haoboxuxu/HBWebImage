b89e7060c9531426277651026a9ef2e2824e8977
haoboxuxu <haoboxuxu@icloud.com>
2021-11-25 20:15:03 +0800

    Initial commit

---
+++ Example/HBWebImage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HBWebImage
+//
+//  Created by haoboxuxu on 11/25/2021.
+//  Copyright (c) 2021 haoboxuxu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
