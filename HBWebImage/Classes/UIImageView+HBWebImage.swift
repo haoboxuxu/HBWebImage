f731e552c89d5eef745e6a8b751de64b20532241
haoboxuxu <haoboxuxu@icloud.com>
2021-12-03 22:55:46 +0800

    update completion on UIImage extension

--- HBWebImage/Classes/UIImageView+HBWebImage.swift
+++ HBWebImage/Classes/UIImageView+HBWebImage.swift
@@ -8,18 +8,22 @@
 import UIKit
 
 extension UIImageView: HBWebImageProtocol {
-    //completionHandler: ((UIImage?) -> Void)
-    public func hbSetImage(with url: URL?, placeholderImage: UIImage) {
+    //
+    public func hbSetImage(with url: URL?, placeholderImage: UIImage? = nil, completion: ((UIImage?) -> Void)? = nil) {
         MainQueue.async {
-            self.image = placeholderImage
+            if let placeholderImage = placeholderImage {
+                self.image = placeholderImage
+            }
         }
         guard let url = url else {
             print("url error")
+            completion?(nil)
             return
         }
         if let cachedImage = HBWebImageCacheManger.fetchCachedImage(urlStr: url.absoluteString) {
             MainQueue.async {
                 self.image = cachedImage
+                completion?(cachedImage)
             }
             return
         }
@@ -30,12 +34,14 @@ extension UIImageView: HBWebImageProtocol {
                 let image = UIImage(data: data)
                 MainQueue.async {
                     self.image = image
+                    completion?(image)
                 }
                 StorageQueue.async {
                     HBWebImageCacheManger.saveImage(data: data, urlStr: url.absoluteString)
                 }
             case .failure(let error):
                 print(error.localizedDescription)
+                completion?(nil)
             }
         }
     }
